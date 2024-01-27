7c20f02c8cc76e963c70518457060a23cbd38831
Garion Herman <cheddam@me.com>
2024-01-27 17:33:53 +0900

    Clean up unnecessary resources, flesh out ModuleData resources

--- scripts/gameplay-ui.gd
+++ scripts/gameplay-ui.gd
@@ -1,13 +1,13 @@
 extends Control
 
 var modules = [
-	load("res://resources/module-datas/top-hat.tres")
+	load("res://resources/module-datas/evil-monkey.tres"),
+	load("res://resources/module-datas/hot-sauce.tres")
 ]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for module in modules:
-		print(module)
 		%modifier_list.add_item(module.name, module.icon)
 		
 	pass # Replace with function body.
@@ -19,10 +19,11 @@ func _process(delta):
 
 
 func _on_item_list_item_clicked(index, at_position, mouse_button_index):
-	print(index)
+	%laughter_module/Laughter.SetPitch(modules[index].pitch)
+	%laughter_module/Laughter.SetSpeed(modules[index].speed)
+	%laughter_module/Laughter.SetVolume(modules[index].volume)
+	%laughter_module/Laughter.Play()
 	
 	# Add module to the list of active modifiers
-	modules[index]
-	$LaughterModule.play()
 	
 	pass # Replace with function body.
