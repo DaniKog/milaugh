c71391a9d2df801b547e5d8f5d90c0f1b1096fa0
Dani Kogan <dani.kogandk@gmail.com>
2024-01-27 18:23:52 +0900

    AudioRefactor

--- scripts/laughter.gd
+++ scripts/laughter.gd
@@ -1,73 +1,73 @@
 extends Node2D
 #naming variables
 var maxVariations = 2
-enum laughterIntensity {Low, Medium, High}
-var intensity = laughterIntensity.Medium
-enum laughterSpeed {Slow, Normal, Fast}
-var speed = laughterSpeed.Normal
+@onready var globals  = get_node("/root/Globals")
+@onready var volume = globals.laughterVolume.Medium
+
+@onready var speed = globals.laughterSpeed.Normal
 var pitch = 1
 
-var speedCount = 3
-var volumeCount = 3
-@onready var audioplayer = $LaughterSound
+@onready var audioPlayer = $LaughterSound
 @onready var laugh = load("res://audio/Laugh_Ha_High_Fast-001.wav")
+@onready var laughBus = AudioServer.get_bus_index("Laugh")
 #@export AudioStream streams: Array  
 # Called when the node enters the scene tree for the first time.
 func _ready():
-
 	pass # Replace with function body.
-
-
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 	
-func RaisePitch():
-	pitch = pitch + 0.5
-	print(pitch)
-	
-func LowerPitch():
-	pitch = pitch - 0.5
-	print(pitch)
-	
-func RaiseSpeed():
-	speed = speed + 1
-	print(laughterSpeed.keys()[speed])
-	
-func LowerSpeed():
-	speed = speed - 1 
-	print(laughterSpeed.keys()[speed])
-
-func RaiseIntensity():
-	intensity = intensity + 1
-	print(intensity)
-	
-func LowerIntensity():
-	intensity = intensity - 1 
-	print(intensity)
-	
-func SetIntensity(intensity_index):
-	intensity = intensity_index
-	
-func SetSpeed(speed_index):
-	speed = speed_index
-	
-func SetPitch(pitch_number):
-	pitch = pitch_number
-	
 func Play():
+	Stop()
 	UpdateLaughSound()
-	audioplayer.play()
+	audioPlayer.play()
 
 func Stop():
-	audioplayer.stop()
+	audioPlayer.stop()
+
+func SetValue(myType, value):
+	match myType:
+		globals.LaughParameter.Pitch:
+			pitch = value*0.5
+			if pitch == 0:
+				pitch = .3
+		globals.LaughParameter.Speed:
+			speed = value
+		globals.LaughParameter.Volume:
+			volume = value
+		_:
+			print('Laugh Paramter not support')
+	UpdateLaughSound()
 	
 func UpdateLaughSound():
 	var formatString = "res://audio/Laugh_Ha_{intensity}_{speed}-00{variation}.wav"
-	var laughSoundPath = formatString.format({"intensity": laughterIntensity.keys()[intensity],
-	 "speed": laughterSpeed.keys()[speed], "variation" : 1})
+	var sample_volume = volume
+	if sample_volume > 3 :
+		sample_volume = 3
+	if sample_volume < 1 :
+		sample_volume = 1
+		
+	var sample_speed = speed
+	if sample_speed > 3 :
+		sample_speed = 3
+	if sample_speed < 1 :
+		sample_speed = 1
+		
+	var laughSoundPath = formatString.format({"intensity": globals.laughterVolume.keys()[sample_volume],
+	 "speed": globals.laughterSpeed.keys()[sample_speed], "variation" : 1})
 	laugh = load(str(laughSoundPath))
 	print(str(laughSoundPath))
-	audioplayer.pitch_scale = pitch
-	audioplayer.stream = laugh
+	print(globals.laughterSpeed.keys()[speed])
+	print(globals.laughterVolume.keys()[volume])
+	print(pitch)
+	
+	audioPlayer.stream = laugh
+	var effect = AudioServer.get_bus_effect(1, 0)
+	if speed == 4:
+		effect.drive = 1
+	else:
+		effect.drive = 0
+		
 
