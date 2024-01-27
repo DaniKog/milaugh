2c9d1bd7dc6437150765a6ab489188ad9e8842c8
Garion Herman <cheddam@me.com>
2024-01-27 22:01:25 +0900

    Fixed item details, added logic to move items to active and enable launch button

--- scripts/gameplay-ui.gd
+++ scripts/gameplay-ui.gd
@@ -16,10 +16,26 @@ var modules = [
 func _ready():
 	for module in modules:
 		%item_list.add_item(module.name, module.icon)
+		%item_list.set_item_tooltip(-1, module.description)
+		%item_list.set_item_tooltip_enabled(-1, true)
 
 func _on_item_list_item_clicked(index, at_position, mouse_button_index):
 	# Add to active item list
-	%active_item_list.add_item(modules[index].name, modules[index].icon)
+	%active_item_list.add_item(
+		%item_list.get_item_text(index),
+		%item_list.get_item_icon(index)
+	)
+	
+	# Remove from available item list
+	%item_list.remove_item(index)
+	
+	# If we've selected 3 items, stop further selections
+	if %active_item_list.item_count >= 3:
+		%button_launch.set_disabled(false)
+		for i in range(%item_list.item_count):
+			%item_list.set_item_disabled(i, true)
+			
+	
 	
 	# Apply value changes
 	%laughter_module/Laughter.AddValue(globals.LaughParameter.Pitch, modules[index].pitch)
@@ -30,3 +46,10 @@ func _on_item_list_item_clicked(index, at_position, mouse_button_index):
 	%laughter_module/Laughter.Play()
 	await get_tree().create_timer(PREVIEW_LENGTH).timeout
 	%laughter_module/Laughter.Stop()
+	
+	
+
+
+func _on_button_launch_pressed():
+	print("go big mode")
+	pass # Replace with function body.
