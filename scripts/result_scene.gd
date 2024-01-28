9e79fb0a7ae757f7d9ee8b3db1496c951d9a011f
Dani Kogan <dani.kogandk@gmail.com>
2024-01-28 16:56:43 +0900

    added Reveal sound

--- scripts/result_scene.gd
+++ scripts/result_scene.gd
@@ -13,8 +13,10 @@ var meh_robot = preload("res://art/robot_meh.png")
 var sad_robot = preload("res://art/robot_sad.png")
 
 @onready var globals  = get_node("/root/Globals")
-
-
+var negative_reveal = preload("res://sfx/negative_reveal.wav")
+var neutral_reveal = preload("res://sfx/neutral_reveal.wave.wav")
+var positive_reveal = preload("res://sfx/positive_reveal.wav")
+@onready var audioPlayer = $AudioStreamPlayer2D
 func _on_visibility_changed():
 	if visible:
 		# Set content based on results
@@ -23,12 +25,16 @@ func _on_visibility_changed():
 		if globals.happy_customers >= SUCCESS_THRESHOLD:
 			%label_summary.text = RESULT_SUCCESS
 			%graphic_result.texture = happy_robot
+			audioPlayer.stream = positive_reveal
 		elif globals.happy_customers >= NEUTRAL_THRESHOLD:
 			%label_summary.text = RESULT_NEUTRAL
 			%graphic_result.texture = meh_robot
+			audioPlayer.stream = neutral_reveal
 		else:
 			%label_summary.text = RESULT_FAILURE
 			%graphic_result.texture = sad_robot
+			audioPlayer.stream = negative_reveal
+		audioPlayer.play()
 
 
 func _on_button_next_pressed():
